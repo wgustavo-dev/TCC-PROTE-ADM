d79b11d64de7f6d00f719ea3663e000c3b48d3a9
GuilhermeSantu <168153877+GuilhermeSantu@users.noreply.github.com>
2026-08-05 02:23:41 -0300

    Sistema de perfil tega

---
+++ Front_end/public/js/perfil-init.js
@@ -0,0 +1,11 @@
+window.__PERFIL_WIDGET_MANUAL_INIT__ = true;
+
+PerfilWidget.init({
+
+    userStorageKey: "prote_user",
+
+    changePasswordUrl: "redefinir_senha.html",
+
+    logoutUrl: "login.html",
+
+});
